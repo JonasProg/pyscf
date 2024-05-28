de76fdbfa6f1ed0da83842c5a3a60772340961a0
Jonas Vester <jonas-vester@hotmail.de>
2024-05-28 15:16:29 +0200

    Change the interface according to the integral driver templates in pyframe

--- pyscf/embedding/embedding.py
+++ pyscf/embedding/embedding.py
@@ -46,6 +46,144 @@ def embedding_for_scf(mf, solvent_obj):
     return _attach_embedding._for_scf(mf, solvent_obj)
 
 
+class EmbeddingIntegralDriver:
+
+    def __init__(self, molecule, coordinates=None):
+        self.mol = molecule
+        if coordinates is not None:
+            self.coordinates0 = coordinates
+            self.coordinates1 = coordinates
+            self.coordinates2 = coordinates
+            fakemol = gto.fakemol_for_charges(coordinates)
+            self.integral0 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e')
+            self.integral1 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ip1')
+            self.integral2_1 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ipip1')
+            self.integral2_2 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ipvip1')
+        else:
+            self.coordinates0 = None
+            self.coordinates1 = None
+            self.coordinates2 = None
+            self.integral0 = None
+            self.integral1 = None
+            self.integral2_1 = None
+            self.integral2_2 = None
+
+    def electronic_fields(self,
+                          coordinates: np.ndarray,
+                          density_matrix: np.ndarray) -> np.ndarray:
+        """Calculate the electronic fields on coordinates.
+
+        Args:
+            coordinates: Coordinates on which the fields are to be evaluated.
+                Shape: (number of atoms, 3)
+                Dtype: np.float64
+            density_matrix: Density Matrix that is the source of the electronic field.
+                Shape: (number of ao functions, number of ao functions)
+                Dtype: np.float64
+
+        Returns:
+            Electronic fields. Shape: (number of atoms, 3) Dtype: np.float64.
+        """
+        if self.coordinates1 is None or not np.array_equal(self.coordinates1, coordinates):
+            self.coordinates1 = coordinates
+            fakemol = gto.fakemol_for_charges(self.coordinates1)
+            self.integral1 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ip1')
+        return -1 * (np.einsum('aijg,ij->ga', self.integral1, density_matrix)
+                     + np.einsum('aijg,ji->ga', self.integral1, density_matrix))
+
+    def multipole_potential_integrals(self,
+                                      multipole_coordinates: np.ndarray,
+                                      multipole_orders: np.ndarray,
+                                      multipoles: list[np.ndarray]) -> np.ndarray:
+        """Calculate the electronic potential integrals and multiply with the multipoles.
+
+        Args:
+            multipole_coordinates: Coordinates of the Multipoles.
+                Shape: (number of atoms, 3)
+                Dtype: np.float64.
+            multipole_orders: Multipole orders of all multipoles.
+                Shape: (number of atoms)
+                Dtype: np.int64
+            multipoles: Multipoles multiplied with degeneracy coefficients and taylor coefficients.
+                Shape: (number of atoms, number of multipole elements)
+                Dtype: np.float64
+
+        Returns:
+            Product of electronic potential integrals and multipoles.
+                Shape: (number of ao functions, number of ao functions)
+                Dtype: np.float64
+        """
+        if np.any(multipole_orders > 2):
+            raise NotImplementedError("""Multipole potential integrals not
+                                             implemented for order > 2.""")
+        op = 0
+        # 0 order
+        idx = np.where(multipole_orders >= 0)[0]
+        charge_coordinates = multipole_coordinates[idx]
+        if not np.array_equal(self.coordinates0, charge_coordinates):
+            self.coordinates0 = charge_coordinates
+            fakemol = gto.fakemol_for_charges(charge_coordinates)
+            self.integral1 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ip1')
+        charges = np.array([m[0:1] for m in multipoles])
+        op += np.einsum('ijg,ga->ij', self.integral0 * -1.0, charges)
+        # 1 order
+        if np.any(multipole_orders >= 1):
+            idx = np.where(multipole_orders >= 1)[0]
+            dipole_coordinates = multipole_coordinates[idx]
+            if not np.array_equal(self.coordinates1, dipole_coordinates):
+                self.coordinates1 = dipole_coordinates
+                fakemol = gto.fakemol_for_charges(self.coordinates1)
+                self.integral1 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ip1')
+            integral1 = self.integral1
+            dipoles = np.array([multipoles[i][1:4] for i in idx])
+            v = np.einsum('aijg,ga->ij', integral1, dipoles)
+            op += v + v.T
+        # 2 order
+        if np.any(multipole_orders >= 2):
+            idx = np.where(multipole_orders >= 2)[0]
+            quadrupol_coordinates = multipole_coordinates[idx]
+            if not np.array_equal(self.coordinates0, quadrupol_coordinates):
+                self.coordinates2 = quadrupol_coordinates
+                fakemol = gto.fakemol_for_charges(quadrupol_coordinates)
+                self.integral2_1 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ipip1')
+                self.integral2_2 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ipvip1')
+            n_sites = idx.size
+            quadrupoles_non_symmetrized = np.array([multipoles[i][4:10] for i in idx])
+            quadrupoles = np.zeros((n_sites, 9))
+            # FIXME build my own matrix.
+            quadrupoles[:, [0, 1, 2, 4, 5, 8]] = quadrupoles_non_symmetrized
+            quadrupoles[:, [0, 3, 6, 4, 7, 8]] += quadrupoles_non_symmetrized
+            quadrupoles *= -0.5
+            v = np.einsum('aijg,ga->ij', self.integral2_1, quadrupoles)
+            op += v + v.T
+            op += np.einsum('aijg,ga->ij', self.integral2_2, quadrupoles) * 2
+        return op
+
+    def induced_dipoles_potential_integrals(self,
+                                            induced_dipoles: np.ndarray,
+                                            coordinates: np.ndarray) -> np.ndarray:
+        """Calculate the electronic potential integrals and contract with the induced dipoles of Atoms.
+
+        Args:
+            induced_dipoles: Induced dipoles
+                Shape (number of induced dipoles, 3)
+                Dtype: np.float64
+            coordinates: Coordinates of the induced dipoles on which the integrals are to be evaluated.
+                Shape (number of induced dipoles, 3)
+                Dtype: np.float64
+
+        Returns:
+            Product of the electronic potential integrals and the induced dipoles.
+                Shape: (number of ao functions, number of ao functions)
+                Dtype: np.float64
+        """
+        if self.coordinates1 is None or not np.array_equal(self.coordinates1, coordinates):
+            self.coordinates1 = coordinates
+            fakemol = gto.fakemol_for_charges(self.coordinates1)
+            self.integral1 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ip1')
+        f_el_ind = np.einsum('aijg,ga->ij', -1 * self.integral1, induced_dipoles)
+        return f_el_ind + f_el_ind.T
+
 class PolarizableEmbedding(lib.StreamObject):
     _keys = {'mol', 'comm', 'options', 'classical_subsystem', 'quantum_subsystem', 'e', 'v'}
 
@@ -64,7 +202,10 @@ class PolarizableEmbedding(lib.StreamObject):
         if not isinstance(self.options, dict):
             raise TypeError("Options should be a dictionary.")
         self._create_pyframe_objects()
-        self._f_el_es = self._compute_multipole_potential()
+        self._integral_driver = EmbeddingIntegralDriver(molecule=self.mol)
+        self._f_el_es = electrostatic_interactions.es_fock_matrix_contributions(
+            classical_subsystem=self.classical_subsystem,
+            integral_driver=self._integral_driver)
         self._e_nuc_es = electrostatic_interactions.compute_electrostatic_nuclear_energy(
             quantum_subsystem=self.quantum_subsystem,
             classical_subsystem=self.classical_subsystem)
@@ -141,11 +282,11 @@ class PolarizableEmbedding(lib.StreamObject):
         self._e_nuc_es = None
         self._e_rep = None
         self._e_disp = None
-        self.e = None
-        self.v = None
         self._dm = None
         self._e_ind = None
         self._e_es = None
+        self.e = None
+        self.v = None
         return self
 
     def kernel(self, dm):
@@ -154,78 +295,34 @@ class PolarizableEmbedding(lib.StreamObject):
         if not (isinstance(dm, np.ndarray) and dm.ndim == 2):
             # spin-traced DM for UHF or ROHF
             dm = dm[0] + dm[1]
-        self._e_ind, self._e_es, v = self._compute_pe_contributions(density_matrices=dm)
+        self._e_ind, self._e_es, v = self._compute_pe_contributions(density_matrix=dm)
         self.e = self._e_ind + self._e_es + self._e_disp + self._e_rep
         self.v = v
         return self.e, self.v
 
     def _create_pyframe_objects(self):
         # should the creation process get a callback and throw back if sth goes wrong?
-        # throw exception if its not exactly for one qm and one classical subsystem
+        # throw exception if it is not exactly for one qm and one classical subsystem
         self.quantum_subsystem, self.classical_subsystem = (read_input.reader(
             input_data=self.options['json_file'],
             comm=self.comm))
 
-    def _compute_multipole_potential(self):
-        if np.any(self.classical_subsystem.multipole_orders > 2):
-            raise NotImplementedError("""Multipole potential integrals not
-                                      implemented for order > 2.""")
-        moments = self.classical_subsystem.degenerate_multipoles_with_taylor_coefficients
-        op = 0
-        # op -= because sign is different compared to vlx?
-        # 0 order
-        fakemol = gto.fakemol_for_charges(self.classical_subsystem.coordinates)
-        integral0 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e')
-        moments_0 = np.array([m[0:1] for m in moments])
-        op -= np.einsum('ijg,ga->ij', integral0, moments_0)
-        # 1 order
-        if np.any(self.classical_subsystem.multipole_orders >= 1):
-            idx = np.where(self.classical_subsystem.multipole_orders >= 1)[0]
-            fakemol = gto.fakemol_for_charges(self.classical_subsystem.coordinates[idx])
-            integral1 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ip1')
-            moments_1 = np.array([moments[i][1:4] for i in idx])
-            v = np.einsum('aijg,ga->ij', integral1, moments_1)
-            op -= v + v.T
-        # 2 order
-        if np.any(self.classical_subsystem.multipole_orders >= 2):
-            idx = np.where(self.classical_subsystem.multipole_orders >= 2)[0]
-            fakemol = gto.fakemol_for_charges(self.classical_subsystem.coordinates[idx])
-            n_sites = idx.size
-            moments_2_non_symmetrized = np.array([moments[i][4:10] for i in idx])
-            moments_2 = np.zeros((n_sites, 9))
-            # FIXME build my own matrix.
-            moments_2[:, [0, 1, 2, 4, 5, 8]] = moments_2_non_symmetrized
-            moments_2[:, [0, 3, 6, 4, 7, 8]] += moments_2_non_symmetrized
-            moments_2 *= .5
-            integral2 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ipip1')
-            v = np.einsum('aijg,ga->ij', integral2, moments_2)
-            op -= v + v.T
-            integral2 = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ipvip1')
-            op -= np.einsum('aijg,ga->ij', integral2, moments_2) * 2
-        return op
-
-    def _compute_pe_contributions(self, density_matrices):
-        density_matrices = np.asarray(density_matrices)
-        nao = density_matrices.shape[-1]
-        density_matrices = density_matrices.reshape(-1, nao, nao)
-        e_el_es = np.einsum('ij,xij->x', self._f_el_es, density_matrices)[0]
-        fakemol = gto.fakemol_for_charges(self.classical_subsystem.coordinates)
-        # first order derivative of the electronic potential integral
-        j3c = df.incore.aux_e2(self.mol, fakemol, intor='int3c2e_ip1')
-        electric_fields = (np.einsum('aijg,ij->ga', j3c, density_matrices[0]) +
-                           np.einsum('aijg,ji->ga', j3c, density_matrices[0]))
-        nuclear_fields = self.quantum_subsystem.compute_nuclear_fields(self.classical_subsystem.coordinates)
-        # Set ind dipoles options here
-        self.classical_subsystem.solve_induced_dipoles(external_fields=(-electric_fields + nuclear_fields),
+    def _compute_pe_contributions(self, density_matrix):
+        density_matrix = np.asarray(density_matrix)
+        nao = density_matrix.shape[-1]
+        density_matrix = density_matrix.reshape(-1, nao, nao)
+        e_el_es = np.einsum('ij,xij->x', self._f_el_es, density_matrix)[0]
+        el_fields = self.quantum_subsystem.compute_electronic_fields(coordinates=self.classical_subsystem.coordinates1,
+                                                                     density_matrix=density_matrix[0],
+                                                                     integral_driver=self._integral_driver)
+        nuc_fields = self.quantum_subsystem.compute_nuclear_fields(self.classical_subsystem.coordinates1)
+        self.classical_subsystem.solve_induced_dipoles(external_fields=(el_fields + nuc_fields),
                                                        threshold=self._threshold,
                                                        max_iterations=self._max_iterations,
                                                        solver=self._solver)
         e_ind = induction_interactions.compute_induction_energy(
-            induced_dipoles=self.classical_subsystem.induced_dipoles.
-            induced_dipoles,
-            total_fields=-electric_fields + nuclear_fields +
-                         self.classical_subsystem.multipole_fields)
-        f_el_ind = np.einsum('aijg,ga->ij', j3c, self.classical_subsystem.induced_dipoles.
-                             induced_dipoles)
-        f_el_ind = f_el_ind + f_el_ind.T
-        return e_ind, self._e_nuc_es + e_el_es, self._f_el_es + f_el_ind
+            induced_dipoles=self.classical_subsystem.induced_dipoles.induced_dipoles,
+            total_fields=el_fields + nuc_fields + self.classical_subsystem.multipole_fields)
+        f_el_ind = induction_interactions.ind_fock_matrix_contributions(classical_subsystem=self.classical_subsystem,
+                                                                        integral_driver=self._integral_driver)
+        return e_ind, self._e_nuc_es + e_el_es, self._f_el_es - f_el_ind
